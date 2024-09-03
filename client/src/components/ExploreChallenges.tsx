33e03408667f8d7333c6a2ccf0497e858c848e41
AmritHimanshu <himanshukumaramrit41855@gmail.com>
2024-09-03 14:07:16 +0530

    added date formatter

--- client/src/components/ExploreChallenges.tsx
+++ client/src/components/ExploreChallenges.tsx
@@ -1,12 +1,62 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import SearchIcon from '@mui/icons-material/Search';
 import TaskAltIcon from '@mui/icons-material/TaskAlt';
 import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
 
+type Challenge = {
+    challenge_name: string;
+    start_date: string;
+    end_date: string;
+    description: string;
+    file: string; // URL of the file
+    level: string;
+};
+
 function ExploreChallenges() {
 
     const [isFilter,setIsFilter] = useState(false);
+    const [challengeList, setChallengeList] = useState<Challenge[]>();
+    console.log(challengeList)
+
+    useEffect(()=>{
+        let listObj: Challenge[] = [];
+        let list = localStorage.getItem("challenge-list");
+        if(list == null) setChallengeList(listObj);
+        else{
+            listObj = JSON.parse(list);
+            setChallengeList(listObj);
+        }
+    },[]);
+
+    function formatDate(dateStr: string): string {
+        const date = new Date(dateStr);
+        
+        const options: Intl.DateTimeFormatOptions = { day: '2-digit', month: 'short', year: '2-digit' };
+        const formatter = new Intl.DateTimeFormat('en-GB', options);
+    
+        const parts = formatter.formatToParts(date);
+        const day = parts.find(part => part.type === 'day')?.value;
+        const month = parts.find(part => part.type === 'month')?.value;
+        const year = parts.find(part => part.type === 'year')?.value;
+
+        const dayWithSuffix = addOrdinalSuffix(day);
+    
+        return `${dayWithSuffix} ${month}'${year}`;
+    };
+    
+    // Function to add ordinal suffix
+    function addOrdinalSuffix(day: string | undefined): string {
+        if (!day) return '';
+        const dayNumber = parseInt(day, 10);
+        if (dayNumber > 3 && dayNumber < 21) return `${day}th`;
+        switch (dayNumber % 10) {
+            case 1: return `${day}st`;
+            case 2: return `${day}nd`;
+            case 3: return `${day}rd`;
+            default: return `${day}th`;
+        }
+    };
 
     return (
         <div>
@@ -47,21 +97,23 @@ function ExploreChallenges() {
             <div className='bg-[#003145] text-white py-16'>
                 <div className='grid grid-cols-3 place-items-center w-[1172px] m-auto gap-y-20 gap-x-10'>
 
-                    <div className='w-[354px] bg-white text-black rounded-xl'>
-                        <div className='h-[176px] overflow-hidden'>
-                            <img src="/assets/cardimage/Group 1000002466.png" alt="" />
+                    {challengeList?.map((challenge,index)=>(
+                        <div key={index} className='w-[354px] bg-white text-black rounded-xl'>
+                            <div className='h-[176px] overflow-hidden'>
+                                <img src="/assets/cardimage/Group 1000002466.png" alt="" />
+                            </div>
+                            <div className='text-center py-7 space-y-4'>
+                                <p className='px-7 py-[1px] bg-[#FF3C00] bg-opacity-20 max-w-min m-auto rounded-sm text-[14px]'>Past</p>
+                                <div className='font-bold w-[244px] m-auto text-[16px]'>{challenge.challenge_name}</div>
+                                <p>Ended on</p>
+                                <p className='text-[18px] font-bold text-gray-600'>{formatDate(challenge.end_date)}</p>
+                                <button className='bg-green-700 text-white text-[16px] font-semibold px-5 py-2 rounded-md space-x-4'>
+                                    <TaskAltIcon sx={{fontSize:'18px'}}/>
+                                    <span>Participate Now</span>
+                                </button>
+                            </div>
                         </div>
-                        <div className='text-center py-7 space-y-4'>
-                            <p className='px-7 py-[1px] bg-[#FF3C00] bg-opacity-20 max-w-min m-auto rounded-sm text-[14px]'>Past</p>
-                            <div className='font-bold w-[244px] m-auto text-[16px]'>Travel Insurance Claim Prediction</div>
-                            <p>Ended on</p>
-                            <p className='text-[18px] font-bold text-gray-600'>16th May'22 09:00 PM</p>
-                            <button className='bg-green-700 text-white text-[16px] font-semibold px-5 py-2 rounded-md space-x-4'>
-                                <TaskAltIcon sx={{fontSize:'18px'}}/>
-                                <span>Participate Now</span>
-                            </button>
-                        </div>
-                    </div>
+                    ))}
 
                 </div>
             </div>
