c79f1c6221bb20f3d34d3159cb4f1096cd9b180f
AmritHimanshu <himanshukumaramrit41855@gmail.com>
2024-09-05 13:02:08 +0530

    made edit challenge responsive

--- client/src/components/EditChallenge.tsx
+++ client/src/components/EditChallenge.tsx
@@ -87,38 +87,38 @@ function EditChallenge() {
     return (
         <div className='relative'>
             {message && (
-                <div className={`sticky top-0 z-50 w-full p-4 text-[20px] text-black font-[500] ${message === "Successfully created" && "bg-green-300" }`}>
+                <div className={`sticky top-0 z-50 w-full p-4 text-[16px] lg:text-[20px] text-black font-[500] ${message === "Successfully created" && "bg-green-300" }`}>
                     {message}
                 </div>
             )}
-            <div className='px-20 py-5 font-semibold text-[18px]'>
+            <div className='px-10 lg:px-20 py-3 lg:py-5 font-semibold text-[18px]'>
             <Link to="/">DPhi</Link>
             </div>
-            <div className='px-20 py-9 font-semibold text-[25px] bg-slate-100'>
+            <div className='px-14 py-3 lg:px-20 lg:py-9 font-semibold text-[16px] lg:text-[25px] bg-slate-100'>
                 Challenge Details
             </div>
-            <form className='px-20 py-10'>
-                <div className='space-y-4 text-[17px] mb-8'>
+            <form className='px-7 py-6 lg:px-20 lg:py-10'>
+                <div className='space-y-2 lg:space-y-4 text-[14px] lg:text-[17px] mb-5 lg:mb-8'>
                     <label htmlFor="challenge_name" className='block font-medium'>Challenge Name</label>
-                    <input type="text" id="challenge_name" name="challenge_name" value={challenge.challenge_name} onChange={handleOnChange} placeholder="Challenge Name" className='border-[1px] border-neutral-400 rounded-md w-[500px] p-2 outline-0' />
+                    <input type="text" id="challenge_name" name="challenge_name" value={challenge.challenge_name} onChange={handleOnChange} placeholder="Challenge Name" className='border-[1px] border-neutral-400 rounded-md w-[300px] lg:w-[500px] p-2 outline-0' />
                 </div>
-                <div className='space-y-4 text-[17px] my-8'>
+                <div className='space-y-2 lg:space-y-4 text-[14px] lg:text-[17px] mb-5 lg:mb-8'>
                     <label htmlFor="start_date" className='block font-medium'>Start Date</label>
-                    <input type="date" id="start_date" name="start_date" value={challenge.start_date} onChange={handleOnChange} placeholder="Add start date" className='border-[1px] border-neutral-400 rounded-md w-[500px] p-2 outline-0' />
+                    <input type="date" id="start_date" name="start_date" value={challenge.start_date} onChange={handleOnChange} placeholder="Add start date" className='border-[1px] border-neutral-400 rounded-md w-[300px] lg:w-[500px] p-2 outline-0' />
                 </div>
-                <div className='space-y-4 text-[17px] my-8'>
+                <div className='space-y-2 lg:space-y-4 text-[14px] lg:text-[17px] mb-5 lg:mb-8'>
                     <label htmlFor="end_date" className='block font-medium'>End Date</label>
-                    <input type="date" id="end_date" name="end_date" value={challenge.end_date} onChange={handleOnChange} placeholder="Add end date" className='border-[1px] border-neutral-400 rounded-md w-[500px] p-2 outline-0' />
+                    <input type="date" id="end_date" name="end_date" value={challenge.end_date} onChange={handleOnChange} placeholder="Add end date" className='border-[1px] border-neutral-400 rounded-md w-[300px] lg:w-[500px] p-2 outline-0' />
                 </div>
-                <div className='space-y-4 text-[17px] my-8'>
+                <div className='space-y-2 lg:space-y-4 text-[14px] lg:text-[17px] mb-5 lg:mb-8'>
                     <label htmlFor="description" className='block font-medium'>Description</label>
-                    <textarea id="description" name="description" value={challenge.description} onChange={handleOnChange} className='resize-none border-[1px] border-neutral-400 rounded-md w-[800px] h-[300px] p-2 outline-0' ></textarea>
+                    <textarea id="description" name="description" value={challenge.description} onChange={handleOnChange} className='resize-none border-[1px] border-neutral-400 rounded-md w-[300px] lg:w-[500px] h-[300px] p-2 outline-0' ></textarea>
                 </div>
-                <div className='space-y-4 text-[17px] my-8'>
+                <div className='space-y-2 lg:space-y-4 text-[14px] lg:text-[17px] mb-5 lg:mb-8'>
                     <label className='block font-medium'>Image</label>
                     <input type="file" id="file" name="file" onChange={handleOnChangeFile} className='border-2'/>
                 </div>
-                <div className='space-y-4 text-[17px] my-8'>
+                <div className='space-y-2 lg:space-y-4 text-[14px] lg:text-[17px] mb-5 lg:mb-8'>
                     <label className='block font-medium'>Level Type</label>
                     <select id="level" name="level" value={challenge.level} className='p-2 bg-transparent border-[1px] border-neutral-400 rounded-md w-[200px] outline-0 cursor-pointer' 
                     onChange={handleOnChange}>
@@ -128,7 +128,7 @@ function EditChallenge() {
                         <option value="Hard" >Hard</option>
                     </select>
                 </div>
-                <button className='bg-green-700 text-white text-[17px] font-semibold px-5 py-3 mt-10 rounded-md' onClick={handleOnSubmit} >{isLoading && <AutorenewIcon className='animate-spin' />} Save Challenge</button>
+                <button className='bg-green-700 text-white text-[17px] font-semibold px-3 lg:px-5 py-2 lg:py-3 mt-6 lg:mt-10 rounded-md' onClick={handleOnSubmit} >{isLoading && <AutorenewIcon className='animate-spin' />} Save Challenge</button>
             </form>
         </div>
     )
